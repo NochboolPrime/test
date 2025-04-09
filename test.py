02325f2389062526c42ee8c1f24fbdd3e4055ee4
NochboolPrime <150124182+DanniilShishkin@users.noreply.github.com>
2025-04-09 18:43:05 +0300

    strucrture

--- test.py
+++ test.py
@@ -1,4 +1,72 @@
+import datetime
+import pytest
+import logging
+import allure
+import json
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.common.by import By
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", default="chrome", choices=["chrome", "firefox"])
+    parser.addoption("--headless", action="store_true")
+    parser.addoption("--executor", action="store", default="127.0.0.1")
+    parser.addoption("--log_level", action="store", default="INFO")
+
+@pytest.fixture()
+def driver(request):
+    executor = request.config.getoption("--executor")
+    browser_name = request.config.getoption("--browser")
+    headless = request.config.getoption("--headless")
+    log_level = request.config.getoption("--log_level")
+
+    logger = logging.getLogger(request.node.name)
+    file_handler = logging.FileHandler(f"logs/{request.node.name}.log")
+    file_handler.setFormatter(logging.Formatter("%(levelname)s %(message)s"))
+    logger.addHandler(file_handler)
+    logger.setLevel(level=log_level)
+
+    logger.info(
+        "===> Test %s started at %s" % (request.node.name, datetime.datetime.now())
+    )
+
+    executor_url = f"http://{executor}:4444/wd/hub"
+
+    if browser_name == "chrome":
+        options = Options()
+        if headless:
+            options.add_argument("headless=new")
+        options.headless = headless
+    elif browser_name == "firefox":
+        options = FirefoxOptions()
+        options.headless = headless
+    else:
+        raise NotImplemented()
+
+    browser = webdriver.Remote(command_executor=executor_url, options=options)
+    allure.attach(
+        name=browser.session_id,
+        body=json.dumps(browser.capabilities),
+        attachment_type=allure.attachment_type.JSON,
+    )
+
+    browser.log_level = log_level
+    browser.logger = logger
+    browser.test_name = request.node.name
+
+    logger.info("Browser %s started" % browser)
+
+    browser.maximize_window()
+
+    def fin():
+        browser.quit()
+        logger.info(
+            "===> Test %s finished at %s" % (request.node.name, datetime.datetime.now())
+        )
+
+    request.addfinalizer(fin)
+    return browser
 
 def test_create_category(driver):
     driver.get('https://demo.opencart.com/admin/')
